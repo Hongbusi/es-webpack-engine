305b090939a5f0e1f7e9804ab1b0c81932186d3a
Hongbusi <q951686662@qq.com>
2022-02-07 14:23:14 +0800

    feat: logger

---
+++ src/webpack.dev.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import express from 'express';
+import webpack from 'webpack';
+import chokidar from 'chokidar';
+
+import WebpackNotifierPlugin from 'es-webpack-notifier';
+import ProgressBarPlugin from 'progress-bar-webpack-plugin';
+
+import logger from './config/logger';
