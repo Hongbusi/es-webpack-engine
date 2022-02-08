ec70a542429ec55bba32dec416b52ca1bb0dd24b
Hongbusi <q951686662@qq.com>
2022-02-08 17:39:02 +0800

    feat: logger

---
+++ src/config/options.js
@@ -0,0 +1,6 @@
+
+const options = {
+  __DEV_SERVER_PORT__: 3030
+}
+
+module.exports = options;
---
+++ src/webpack.dev.config.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
+
+const logger = require('./config/logger');
+const options = require('./config/options');
+
+const app = express();
+
+const compiler = webpack({
+  // webpack options
+});
+
+app.use(middleware(compiler, {
+  // webpack-dev-middleware options
+}));
+
+app.listen(options.__DEV_SERVER_PORT__, (err) => {
+  logger.info(`Example server listening on ${options.__DEV_SERVER_PORT__} in ${app.settings.env} node.`);
+
+  if (err) {
+    logger.error(err);
+  }
+});
