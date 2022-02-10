9a61d48f5c76dff466f3655f467c8a84c3fd0572
Hongbusi <q951686662@qq.com>
2022-02-10 15:13:22 +0800

    feat: middleware

---
+++ src/config/middleware.js
@@ -0,0 +1,27 @@
+const WebpackDevMiddleware = require('webpack-dev-middleware');
+
+const options = require('./options');
+
+module.exports = (compiler, publicPath) => {
+  const webpackDevMiddlewareOptions = {
+    publicPath,
+    // quiet: false,
+    // noInfo: false,
+    // progress: true,
+    // stats: {
+    //   colors: true,
+    //   hash: false,
+    //   chunks: false,
+    //   chunkModules: false,
+    //   children: options.__VERBOSE__,
+    //   errorDetails: true
+    // },
+    // watchOptions: {
+    //   aggregateTimeout: 1000,
+    //   poll: 1000
+    // },
+    // lazy: false
+  };
+
+  return webpackDevMiddleware(compiler, webpackDevMiddlewareOptions);
+}
