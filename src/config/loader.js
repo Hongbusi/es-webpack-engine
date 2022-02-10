86bfc5efde1efe9f84f01d7dddd41dbb0c21cf00
Hongbusi <q951686662@qq.com>
2022-02-10 15:10:53 +0800

    feat: loader

---
+++ src/config/loader.js
@@ -0,0 +1,110 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const imageLoader = (path, imgName, limit) => {
+  return {
+    test: /\.(png|jpe?g|gif)$/,
+    type: 'asset',
+    generator: {
+      filename: `${path}/${imgName}/[name][ext]`
+    },
+    parser: {
+      dataUrlCondition: {
+        maxSize: limit
+      }
+    }
+  }
+};
+
+const fontLoader = (path, fontName, limit) => {
+  return {
+    test: /\.(woff|woff2|eot|ttf|svg)(\?(.*))?$/,
+    type: 'asset',
+    generator: {
+      filename: `${path}/${fontName}/[name][ext]`
+    },
+    parser: {
+      dataUrlCondition: {
+        maxSize: limit
+      }
+    }
+  }
+};
+
+const mediaLoader = (path, name) => {
+  return {
+    test: /\.(swf|wav|mp3|mpeg|mp4|webm|ogv)(\?v=\d+\.\d+\.\d+)?$/,
+    type: 'asset/resource',
+    generator: {
+      filename: `${path}/${name}/[name][ext]`
+    }
+  }
+};
+
+const jsLoader = (options, exclude) => {
+  return {
+    test: /\.js[x]?$/,
+    use: [
+      {
+        loader: 'thread-loader',
+        options: {
+          workers: options.cpuNumber
+        }
+      },
+      'babel-loader'
+    ],
+    exclude
+  }
+};
+
+const cssLoader = options => {
+  return {
+    test: /\.css$/,
+    use: [
+      {
+        loader:  options.__DEV__ || options.__DEBUG__  ?  'vue-style-loader' : MiniCssExtractPlugin.loader,
+        options
+      },
+      'css-loader'
+    ]
+  }
+};
+
+const lessLoader = options => {
+  return {
+    test: /\.less$/,
+    use: [
+      {
+        loader:  options.__DEV__ || options.__DEBUG__  ?  'vue-style-loader' : MiniCssExtractPlugin.loader,
+        options
+      },
+      'css-loader',
+      'less-loader'
+    ]
+  }
+};
+
+const vueLoader = options => {
+  return {
+    test: /\.vue$/,
+    loader: 'vue-loader',
+    options
+  }
+}
+
+const importsLoader = regExp => {
+  return {
+    test: new RegExp(`(${regExp.join('|')})$`),
+    loader: 'imports-loader?define=>false&module=>false&exports=>false&this=>window'
+  }
+};
+
+module.exports = {
+  imageLoader,
+  fontLoader,
+  mediaLoader,
+  jsLoader,
+  cssLoader,
+  lessLoader,
+  vueLoader,
+  importsLoader
+}
