8112e1cb48bad62076888c31ce9c52838bdd4628
Hongbusi <q951686662@qq.com>
2022-02-10 16:12:21 +0800

    fix: webpack-merge import fail

--- src/webpack.base.js
+++ src/webpack.base.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const { VueLoaderPlugin } = require('vue-loader');
 const WebpackBar = require('webpackbar')
